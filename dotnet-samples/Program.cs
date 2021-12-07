169a2c2f1ef29f5f4e0eb5e64a3733568633e07e
vsnrchg <vsnrchg@gmail.com>
2021-12-07 13:46:55 +0530

    Vinod|Initial getworkItem sample copied from msdn

---
+++ dotnet-samples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dotnet_samples
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GetAndPrintWorkItems();
+            Console.WriteLine("after calling GetAndPrintWorkItems()");
+            Console.ReadLine();
+        }
+
+        static async void GetAndPrintWorkItems(){
+            QueryExecutor qe = new QueryExecutor("vsnrchg","jnq3olrltj5ktkjvltm6pv4rkjrjegom2kz2u7r6334qchs3vr3q");
+            await qe.PrintOpenBugsAsync("toggler"); 
+        }
+    }
+}
